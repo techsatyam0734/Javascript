4ed9cd80a4c16cd1524fbedb779e55a0fff232a1
unknown <satyamtiwari0734@gmail.com>
2026-08-05 03:05:13 -0700

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 101
+let accountEmail = "satyam0734@example.com"
+var accountPassword = 12345
+accountCity = "Mumbai"
+
+// accountId = 102 // not allowed
+
+console.log([accountId,accountEmail,accountPassword,accountCity]);
+
+// var - functional-scoped
+// let and const - block scoped
+// In const initialization and declaration must be done at the same time and cannot be reassigned a new value
+// In let initialization can be done first and later on we can assign a value to it
+
+// prefer not use var 
