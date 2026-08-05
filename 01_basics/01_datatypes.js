506833a0b839c0257ee369440c9e129bf74eb8eb
unknown <satyamtiwari0734@gmail.com>
2026-08-05 03:07:27 -0700

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "Satyam"
+let age = 23
+let isLoggedIn = true
+
+// number - 2 to power 53
+// bigint 
+// string - "" or ''
+// boolean - true or false
+// null - standalone value
+// undefined
+
+// console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
